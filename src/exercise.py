dc92d7d772aee03102d64b63646e25118a4257cb
pmatyus <>
2021-04-15 10:55:11 +0000

    Averaged three numbers

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    first = int(input('Give the first number:'))
+    second = int(input('Give the second number:'))
+    third = int(input('Give the third number:'))
+    print('The average is ' + str((first + second + third)/3))
 
 if __name__ == '__main__':
     main()
